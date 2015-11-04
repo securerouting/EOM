db17d4b6dc0a7bdd9e2e89875c8def47cb451cad
Suresh Krishnaswamy <suresh@tislabs.com>
2015-11-03 21:39:25 -0500

    Make get_ip_str() a static method of EOMGenericPoller

--- eom/generic_poller.py
+++ eom/generic_poller.py
@@ -7,6 +7,8 @@ and the maintenance of the event log associated with all poll
 operations.
 """
 
+import netaddr
+
 class EOMGenericPoller:
     """A generic poller"""
     def __init__(self, conf, aggregator):
@@ -31,3 +33,28 @@ class EOMGenericPoller:
         This method must be overridden by sub-classes.
         """
         pass
+
+    @staticmethod
+    def get_ip_str(ipint):
+        """Convert an IP int to an ordinal IP string.
+
+        Convert the IP int first to its dotted or string form. Then
+        expand each component so that determining ranges between two of
+        these strings becomes possible.
+
+        Args:
+            ipint: int value of an IP address
+
+        Returns:
+            Expanded string representation.
+        """
+        ipaddr = None
+        ipobj = netaddr.IPAddress(ipint)
+        if ipobj.version == 6:
+            ipaddr = ipobj.format(netaddr.ipv6_verbose)
+            ipaddr = ipaddr.upper()
+        else:
+            dotted = str(ipobj).split(".")
+            ipaddr = '.'.join([ str(i).zfill(3) for i in dotted ])
+        return ipaddr
+
