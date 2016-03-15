0a0aad63516a8be28b8a57d1bb1a62191c9c50d2
Suresh Krishnaswamy <suresh@tislabs.com>
2016-03-15 12:44:45 -0400

    Separate parsing functionality into a new module

---
+++ eom/argparser.py
@@ -0,0 +1,61 @@
+"""Implementation of the analyzer module for EOM.
+
+This module controls most of the EOM tool's operation. It accepts
+preferences from the user, translates that data to configuration
+parameters for the various modules, uses an internal scheduler to invoke
+the fetch routines for various fetcher modules, extracts pertinent
+streams of data from the aggregator module and other data stores, and
+feeds such data to data analysis modules for further processing and
+report generation. 
+"""
+
+import argparse
+
+def rpki_server_spec(s):
+    """Define the format for the rpki-server spec argument"""
+    try:
+        proto, host, port, force, reset = map(str, s.split(':'))
+        n = argparse.Namespace()
+        n.protocol = proto
+        n.host = host
+        n.port = int(port)
+        # There is no database per channel, but we need to define the
+        # parameter in the namespace
+        n.sql_database = None
+        if force == '-1':
+            n.force_version = None 
+        else:
+            n.force_version = int(force) 
+            if n.force_version not in PDU.version_map:
+                raise argparse.ArgumentTypeError("Unrecognized Version")
+        n.reset_session = True if reset == 'Y' else False 
+        return n
+    except:
+        raise argparse.ArgumentTypeError("Format must be protocol:host:port:forceversion:reset")
+             
+def rtr_rib_spec(s):
+    """Define the format for the rtr-rib spec argument"""
+    try:
+        device, poll_interval,reset,realm = map(str, s.split(':'))
+        n = argparse.Namespace()
+        n.device = device
+        n.poll_interval = int(poll_interval)
+        n.reset_session = True if reset == 'Y' else False 
+        n.realm = realm
+        return n
+    except:
+        raise argparse.ArgumentTypeError("Format must be device:poll_interval:reset")
+
+def eom_parse_args(desc):
+    parser = argparse.ArgumentParser(description = desc)
+    parser.add_argument("--sql-database", help = "filename for sqlite3 database of EOM state")
+    parser.add_argument("--rpki-serv", help="RPKI server spec", action="append", type=rpki_server_spec)
+    parser.add_argument("--rtr-rib", help="Router spec", action="append", type=rtr_rib_spec)
+    parser.add_argument("--debug", action = "store_true", help = "debugging mode")
+    parser.add_argument("--log-level", default = "debug", 
+                        choices = ("debug", "info", "warning", "error", "critical"),
+                        type = lambda s: s.lower())
+    parser.add_argument("--log-to", choices = ("syslog", "stderr"))
+    parser.add_argument("--continuous", action = "store_true", help = "Keep polling continuously")
+    return parser
+
