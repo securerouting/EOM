3dfc6df82a4a5c45579720462dc5d43caffeddcc
Suresh Krishnaswamy <suresh@tislabs.com>
2016-03-28 11:07:10 -0400

    Fetch report detail through its hash

--- eom_ui/status/views.py
+++ eom_ui/status/views.py
@@ -14,7 +14,10 @@ def index(request):
 
 def detail(request, rep_id):
 #    routes = ReportDetail.objects.all()
-    routes = ReportDetail.objects.filter(report_id=rep_id)
+    routes = []
+    ri = ReportIndex.objects.get(report_id=rep_id)
+    if ri:
+        routes = ReportDetail.objects.filter(report_hash=ri.report_hash)
     # Prep fields before display
     for r in routes:
         r.path = u'\u27a1'.join(r.pathbutone.split(' ')) + u'\u27a1' + r.orig_asn
